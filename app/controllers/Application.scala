b5759a2c862ad82b91d764916561449cebcc69b5
Julien Richard-Foy <julien@richard-foy.fr>
2013-02-27 20:04:56 +0100

    Handle fetch errors

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,29 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Result, Action, Controller}
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.cache.Cached
+import play.api.cache.Cache
 import play.api.Play.current
 import models.Modules
 
 object Application extends Controller {
   
-  val index = Cached(_ => "modules", duration = 60 * 60) {
-    Action {
-      Async {
-        WS.url("http://raw.github.com/playframework/Play20/master/documentation/manual/Modules.md").get()
-          .map(result => Ok(views.html.modules(Modules.parse(result.body))))
+  val index = Action {
+      Cache.getAs[Result]("modules").getOrElse {
+        Async {
+          WS.url("http://raw.github.com/playframework/Play20/master/documentation/manual/Modules.md").get()
+            .map { githubResult =>
+              if (githubResult.status < 400) {
+                val result = Ok(views.html.modules(Modules.parse(githubResult.body)))
+                Cache.set("modules", result, 60 * 60 * 3) // 3 hours
+                result
+              } else Ok(views.html.oops())
+            } recover {
+              case _ => Ok(views.html.oops())
+            }
+        }
       }
-    }
   }
   
 }
