6e5a787bafabc703b5779aff3c7334795b7cc006
Julien Richard-Foy <julien@richard-foy.fr>
2013-02-28 21:22:06 +0100

    Factor out cache code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import play.api.mvc.{Result, Action, Controller}
+import play.api.mvc.{RequestHeader, Result, Action, Controller}
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.cache.Cache
@@ -10,29 +10,38 @@ import models.Modules
 object Application extends Controller {
   
   val index = Action { implicit request =>
-    val notModified = for {
-      requestEtag <- request.headers.get(IF_NONE_MATCH)
-      etag <- Cache.getAs[String]("etag")
-      if etag == requestEtag
-    } yield NotModified
-    notModified.getOrElse {
-      Cache.getAs[Result]("modules").getOrElse {
-        Async {
+    Cached("modules", 60 * 60 * 3) { // 3 hours
+      Async {
+        {
           WS.url("http://raw.github.com/playframework/Play20/master/documentation/manual/Modules.md").get()
-            .map { githubResult =>
-              if (githubResult.status < 400) {
-                val etag = System.currentTimeMillis().toString
-                val result = Ok(views.html.modules(Modules.parse(githubResult.body))).withHeaders(ETAG -> etag)
-                Cache.set("modules", result, 60 * 60 * 3) // 3 hours
-                Cache.set("etag", etag)
-                result
-              } else InternalServerError(views.html.oops())
-            } recover {
-              case _ => InternalServerError(views.html.oops())
-            }
+        } collect {
+          case response if response.status < 300 => Ok(views.html.modules(Modules.parse(response.body)))
+        } recover {
+          case _ => InternalServerError(views.html.oops())
         }
       }
     }
   }
-  
+
+  def Cached(key: String, duration: Int)(result: => Result)(implicit request: RequestHeader): Result = {
+
+    val resultEtag = s"${key}_etag"
+    val resultKey = s"${key}_result"
+
+    val notModified = for {
+      requestEtag <- request.headers.get(IF_NONE_MATCH)
+      etag <- Cache.getAs[String](resultEtag)
+      if etag == requestEtag
+    } yield NotModified
+
+    notModified.getOrElse {
+      Cache.getAs[Result](resultKey).getOrElse {
+        val etag = System.currentTimeMillis().toString
+        val taggedResult = result.withHeaders(ETAG -> etag)
+        Cache.set(resultKey, taggedResult, duration)
+        Cache.set(resultEtag, etag)
+        taggedResult
+      }
+    }
+  }
 }
